c3b7fb220253be82059c86f391a525de7cd3165e
Larry Siden, Westside Consulting LLC <lsiden@westside-consulting.com>
2011-12-27 11:42:40 -0500

    Replaced Given with When

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,45 +1,45 @@
-Given /^page has "([^"]*)"$/ do |arg1|
+When /^page has "([^"]*)"$/ do |arg1|
   #STDERR.puts page.body
   page.body.should have_content(arg1)
 end
 
-Given /^"([^"]*)" registers$/ do |arg1|
+When /^"([^"]*)" registers$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" receives email with register\-confirmation link$/ do |arg1|
+When /^"([^"]*)" has received email with register\-confirmation link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" is suspended$/ do |arg1|
+When /^"([^"]*)" is suspended$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" visits registration\-confirmation link$/ do |arg1|
+When /^"([^"]*)" visits registration\-confirmation link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" is authenticated$/ do |arg1|
+When /^"([^"]*)" is authenticated$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" e\-mail is confirmed$/ do |arg1|
+When /^"([^"]*)" e\-mail is confirmed$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^administrator receives email with registration notification$/ do
+When /^administrator receives email with registration notification$/ do
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" can change password to "([^"]*)"$/ do |arg1, arg2|
+When /^"([^"]*)" can change password to "([^"]*)"$/ do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^register\-confirmation link will return page\-not\-found$/ do
+When /^register\-confirmation link will return page\-not\-found$/ do
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" submits request to reset password$/ do |arg1|
+When /^"([^"]*)" submits request to reset password$/ do |arg1|
   visit '/forgot-password'
   with_scope('form.forgot_password') do
     fill_in('email', :with => arg1)
@@ -48,7 +48,7 @@ Given /^"([^"]*)" submits request to reset password$/ do |arg1|
 end
 
 # Javascipt injection example
-Given /^{([^}]*)} submits request to reset password$/ do |arg1|
+When /^{([^}]*)} submits request to reset password$/ do |arg1|
   visit '/forgot-password'
   with_scope('form.forgot_password') do
     fill_in('email', :with => arg1)
@@ -56,75 +56,75 @@ Given /^{([^}]*)} submits request to reset password$/ do |arg1|
   end
 end
 
-Given /^"([^"]*)" receives e\-mail with reset\-password link$/ do |arg1|
+When /^"([^"]*)" receives e\-mail with reset\-password link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" visits reset\-password link$/ do |arg1|
+When /^"([^"]*)" visits reset\-password link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" submits request to change e\-mail to "([^"]*)"$/ do |arg1, arg2|
+When /^"([^"]*)" submits request to change e\-mail to "([^"]*)"$/ do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" receives email with change\-email\-confirmation link$/ do |arg1|
+When /^"([^"]*)" receives email with change\-email\-confirmation link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" receives email with change\-email\-notification message and dispute\-change\-email link$/ do |arg1|
+When /^"([^"]*)" receives email with change\-email\-notification message and dispute\-change\-email link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" visits dispute\-change\-email link$/ do |arg1|
+When /^"([^"]*)" visits dispute\-change\-email link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" receives email with dispute notification$/ do |arg1|
+When /^"([^"]*)" receives email with dispute notification$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" receives email with dispute confirmation$/ do |arg1|
+When /^"([^"]*)" receives email with dispute confirmation$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^administrator receives email with dispute confirmation$/ do
+When /^administrator receives email with dispute confirmation$/ do
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" has already confirmed her registration$/ do |arg1|
+When /^"([^"]*)" has already confirmed her registration$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" visits stale registration\-confirmation link$/ do |arg1|
+When /^"([^"]*)" visits stale registration\-confirmation link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^response is redirected to "([^"]*)"$/ do |arg1|
+When /^response is redirected to "([^"]*)"$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^"([^"]*)" has already visited reset\-password link$/ do |arg1|
+When /^"([^"]*)" has already visited reset\-password link$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^\w+ visits? "([^"]*)"$/ do |arg1|
+When /^\w+ visits? "([^"]*)"$/ do |arg1|
   visit arg1
 end
 
-Given /^that "([^"]*)" is authenticated$/ do |arg1|
+When /^that "([^"]*)" is authenticated$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^I should see raw html: "([^"]*)"$/ do |arg1|
+When /^I should see raw html: "([^"]*)"$/ do |arg1|
   page.html.should match /#{arg1}/ 
 end
 
-Given /^"([^"]*)" is registered$/ do |arg1|
+When /^"([^"]*)" is registered$/ do |arg1|
   Authenticatable::Account.all( :email => arg1 ).should have(1).item
 end
 
-Given /^"([^"]*)" is not registered$/ do |arg1|
+When /^"([^"]*)" is not registered$/ do |arg1|
   Authenticatable::Account.all( :email => arg1 ).should have(0).items
 end
 
